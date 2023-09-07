2351903d5a7f3154bf53c3e95a919f84216f642a
Maxim Karassev <maximus_1901@mail.ru>
2023-09-08 00:07:11 +0600

    "Hello" button feature branch

--- app/src/main/java/com/example/firstappandersen/MainActivity.kt
+++ app/src/main/java/com/example/firstappandersen/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.firstappandersen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textHelloWorld = findViewById<View>(R.id.textView)
+        val buttonHelloAndersen = findViewById<View>(R.id.hello_button)
+
+        buttonHelloAndersen.setOnClickListener {
+            textHelloWorld.isVisible = true
+        }
+
     }
 }
